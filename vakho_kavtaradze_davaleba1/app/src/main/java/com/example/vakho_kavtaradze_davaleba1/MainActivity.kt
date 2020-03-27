c57f3f2a38d83a0cb52108390b62115dc2029669
vakho <vakho@example.com>
2020-03-28 03:06:20 +0400

    davaleba 1

---
+++ vakho_kavtaradze_davaleba1/app/src/main/java/com/example/vakho_kavtaradze_davaleba1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.vakho_kavtaradze_davaleba1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        button.setOnClickListener{
+            if(email.text.toString().isEmpty() ){
+                Toast.makeText(this,"You are missing Email Field",Toast.LENGTH_SHORT).show()
+            }
+            if(password.text.toString().isEmpty()){
+                Toast.makeText(this,"You are missing Password Field",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Congrats you are logined in",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
